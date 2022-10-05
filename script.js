5a4eaab46f921cdadfa8472ba55f0a814793f0f6
Daniel Packer <danielapacker@gmail.com>
2022-10-05 12:07:54 -0700

    add running clock

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var customCurrentTime = moment().format("dddd, MMMM Do, YYYY, HH:mm:ss a");
 $(".custom-current-time").text(customCurrentTime);
 console.log(customCurrentTime);
+
+function runningTime() {
+    customCurrentTime = moment().format("dddd, MMMM Do, YYYY, HH:mm:ss a");
+    $(".custom-current-time").text(customCurrentTime);
+}
+
+setInterval(runningTime, 1000);
