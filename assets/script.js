f5fa1e541e233cddce3297ed79a9172799b6f47b
Dempoleon <dempoleon@gmail.com>
2022-10-05 16:34:26 -0700

    modal adds table entries but does not get input text values

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,57 @@ function runningTime() {
 }
 
 setInterval(runningTime, 1000);
+
+$('btn').on('click', modalHandler)
+
+function modalHandler () {
+    var projectName = $('#projectName').value();
+    var projectType = $('#projectType').value();
+    var dueDate = $('#dueDate').value();
+    var hourlyRate = $('#hourlyRate').value();
+
+    console.log(projectName);
+    console.log(projectType);
+
+    console.log(dueDate);
+
+    console.log(hourlyRate);
+
+}
+
+$('#savebtn').on('click', saveProject)
+
+function saveProject() {
+    trEl = $('<tr>');
+    tdProjectName = $('<td>');
+    tdProjectName.val($('#projectName').value);
+    tdProjectType = $('<td>');
+    tdProjectType.text($('#projectType').value);
+
+    //Get
+    var projectNameStr = $('#projectName').val();
+
+    //Set
+    tdProjectName.val(projectNameStr);
+
+    console.log(projectNameStr);
+
+    tdHourRate = $('<td>');
+    tdHourRate.text($('#hourlyRate').value);
+
+    tdDueDate = $('<td>');
+    tdDueDate.text($('#dueDate').value);
+
+    tdDaysLeft = $('<td>');
+    tdEarnings = $('<td>');
+
+    trEl.append(tdProjectName);
+    trEl.append(tdProjectType);
+    trEl.append(tdDueDate);
+    trEl.append(tdHourRate);
+    trEl.append(tdDaysLeft);
+    trEl.append(tdEarnings);
+
+    $('tbody').append(trEl);
+
+}
